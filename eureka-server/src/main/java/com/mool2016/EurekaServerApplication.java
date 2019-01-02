a3f605b4715ec50a523c68897dfc25ca5a5b7b22
jinda_zhangql <jinda_zhangql@git.cloud.tencent.com>
2019-01-02 11:53:03 +0800

    eureka

---
+++ eureka-server/src/main/java/com/mool2016/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.mool2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
