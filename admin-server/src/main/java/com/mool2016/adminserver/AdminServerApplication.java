0be49d68087d842dfbd8dd2c9983b0ab8c2e3ea1
zhangql <251087279@qq.com>
2019-01-25 19:27:15 +0800

    引入spring-boot-admin~

--- admin-server/src/main/java/com/mool2016/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/mool2016/adminserver/AdminServerApplication.java
@@ -1,5 +1,6 @@
 package com.mool2016.adminserver;
 
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-ribbon/src/main/java/com/mool2016/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/mool2016/serviceribbon/ServiceRibbonApplication.java
@@ -2,14 +2,12 @@ package com.mool2016.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableEurekaClient
 public class ServiceRibbonApplication {
 
